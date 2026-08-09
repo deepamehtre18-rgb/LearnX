0b2fc46fcb100062fe0fd9c0b566d77dbcfefdbc
Deepa <deepamehtre18@gmail.com>
2026-08-09 16:17:10 +0530

    Fix database connection

--- src/db/DBConnection.java
+++ src/db/DBConnection.java
@@ -4,18 +4,23 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
+
     private static final String URL = "jdbc:mysql://localhost:3306/learnx_db";
     private static final String USERNAME = "root";
-    private static final String PASSWORD = "system";
+   private static final String PASSWORD = System.getenv("MYSQL_PASSWORD");
 
     public static Connection getConnection() {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            
-            Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
+            Connection connection =
+                    DriverManager.getConnection(URL, USERNAME, PASSWORD);
+
             System.out.println("Database Connected Successfully!");
             System.out.println("Database = " + connection.getCatalog());
+
             return connection;
+
         } catch (Exception e) {
             System.out.println("Database Connection Failed!");
             e.printStackTrace();
